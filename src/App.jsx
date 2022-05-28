639ceaf4f600bd9c7d2eeac0944ae87111178d8a
anik <anikbhadra01@gmail.com>
2022-05-28 11:51:33 +0530

    function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Car_class from '../Components/Car_class'
+import Football_function from '../Components/Football_function'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Car_class />
+        <Football_function />
       </header>
     </div>
   )
