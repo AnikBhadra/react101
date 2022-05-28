eba301b1eb296b8e2a180145417ed0b29ff84cc8
anik <anikbhadra01@gmail.com>
2022-05-28 12:34:27 +0530

    create jsx

---
+++ Components/Create_jsx.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Create_jsx() {
+    return (
+        <div>
+            <h1>React is {5 + 5} times better with JSX</h1>
+        </div>
+    )
+}
+
+export default Create_jsx
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Create_jsx from '../Components/Create_jsx'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
+    <Create_jsx />
     <React.StrictMode>
       <App />
     </React.StrictMode>
