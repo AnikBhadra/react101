b1abfce646cd87623aefb448a0ca96d7e7eda6d7
anik <anikbhadra01@gmail.com>
2022-05-28 16:18:55 +0530

    grid_auto_fill

---
+++ Components/Grid_auto_fill.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Grid_auto_fill() {
+    return (
+        <div>
+            <div class="grid auto-fill">
+                <div>fill</div>
+                <div>fill</div>
+                <div>fill</div>
+                <div>fill</div>
+                <div>fill</div>
+                <div>fill</div>
+                <div>fill</div>
+            </div>
+        </div>
+    )
+}
+
+export default Grid_auto_fill
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import Car_class from '../Components/Car_class'
-import Football_function from '../Components/Football_function'
-import Name from '../Components/Name'
+import Grid_auto_fill from '../Components/Grid_auto_fill'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Car_class />
-        <Football_function />
-        <Name n="Anik" />
-      </header>
+    <div >
+      <Grid_auto_fill />
     </div>
   )
 }
