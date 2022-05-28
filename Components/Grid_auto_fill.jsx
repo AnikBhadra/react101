d6adcaa262d363269e5eaee572a439c0d5d71de0
anik <anikbhadra01@gmail.com>
2022-05-28 16:22:17 +0530

    grid_auto_fit

--- Components/Grid_auto_fill.jsx
+++ Components/Grid_auto_fill.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Grid_auto_fit from './Grid_auto_fit'
 
 function Grid_auto_fill() {
     return (
@@ -12,6 +13,7 @@ function Grid_auto_fill() {
                 <div>fill</div>
                 <div>fill</div>
             </div>
+            <Grid_auto_fit />
         </div>
     )
 }
---
+++ Components/Grid_auto_fit.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Grid_auto_fit() {
+    return (
+        <div>
+            <div class="grid auto-fit">
+                <div>fit</div>
+                <div>fit</div>
+                <div>fit</div>
+                <div>fit</div>
+                <div>fit</div>
+                <div>fit</div>
+                <div>fit</div>
+            </div>
+        </div>
+    )
+}
+
+export default Grid_auto_fit
