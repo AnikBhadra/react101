a1c87dde732a3d331a5e6dbca063479ab8489ba7
anik <anikbhadra01@gmail.com>
2022-06-02 20:02:06 +0530

    run effects only once (class & function)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import ClassCounterOne from "../Components/ClassCounterOne";
-import HookCounterOne from "../Components/HookCounterOne";
+import ClassMouse from "../Components/ClassMouse";
+import HookMouse from "../Components/HookMouse";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounterOne /> */}
-      <HookCounterOne />
+      <HookMouse />
+      {/*  <ClassMouse /> */}
     </div>
   );
 }
