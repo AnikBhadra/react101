3ec26232cf29e05f137190bbb615838dddefce23
anik <anikbhadra01@gmail.com>
2022-06-14 09:33:21 +0530

    fetching data with useEffect part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import FetchingData_useEffect_1 from "../Components/FetchingData_useEffect_1";
+import FetchingData_useEffect_2 from "../Components/FetchingData_useEffect_2";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <FetchingData_useEffect_1 />
-
+      <FetchingData_useEffect_2 />
     </div>
   );
 }
