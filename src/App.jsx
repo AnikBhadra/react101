c3bdacf202dfdb892e08c2be8dab94844595248b
anik <anikbhadra01@gmail.com>
2022-06-28 10:14:14 +0530

    useReducer (simple state & action)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
-import ComponentC from "../Components/ComponentC"
+import CounterOne from "../Components/CounterOne";
 
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div>
-      <UserContext.Provider value={'Anik'}>
-        <ChannelContext.Provider value={"Code"}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
-
+      <CounterOne />
     </div>
   )
 }
