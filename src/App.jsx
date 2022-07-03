367e5a6479af9cb2a4607ad99fc9d2e3e5808fa6
anik <anikbhadra01@gmail.com>
2022-07-03 20:32:04 +0530

    useRef Hook part1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Counters from "../Components/Counters";
+import FocusInputO from "../Components/FocusInputO";
 import "./App.css";
 
 
 function App() {
   return (
     <div>
-      <Counters />
+      <FocusInputO />
     </div>
   )
 }
